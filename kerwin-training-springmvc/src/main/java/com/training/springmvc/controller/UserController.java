07893c3c2164ee47a3df677f8f6b713adf5e51ad
季晴川 <jiqingchuan@b5m.com>
2016-04-15 19:02:21 +0800

    commit

---
+++ kerwin-training-springmvc/src/main/java/com/training/springmvc/controller/UserController.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.training.springmvc.controller;
+
+/**
+ * @description:
+ * Copyright 2011-2015 B5M.COM. All rights reserved
+ * @author: jiqingchuan
+ * @version: 1.0
+ * @createdate: 2016年4月15日
+ * Modification  History:
+ * Date         Author        Version        Discription
+ * -----------------------------------------------------------------------------------
+ * 2016年4月15日       jiqingchuan          1.0             Why & What is modified
+ */
+public class UserController {
+
+}
