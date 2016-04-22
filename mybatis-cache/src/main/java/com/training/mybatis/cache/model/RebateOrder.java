6cea7319828e58b9990f6ef19da200756caa9c5e
季晴川 <jiqingchuan@b5m.com>
2016-04-22 18:54:00 +0800

    add mybatis cache model

---
+++ mybatis-cache/src/main/java/com/training/mybatis/cache/model/RebateOrder.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.training.mybatis.cache.model;
+
+/**
+ * @description:
+ * Copyright 2011-2015 B5M.COM. All rights reserved
+ * @author: jiqingchuan
+ * @version: 1.0
+ * @createdate: 2016年4月21日
+ * Modification  History:
+ * Date         Author        Version        Discription
+ * -----------------------------------------------------------------------------------
+ * 2016年4月21日       jiqingchuan          1.0             Why & What is modified
+ */
+public class RebateOrder {
+
+	/**
+	 * 
+	 */
+	public RebateOrder() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
