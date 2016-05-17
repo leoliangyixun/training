55a54d1ea70bbb0fe856a1df6df02815ea2048a4
leoliangyixun <leoliangyixun@163.com>
2016-05-17 16:49:12 +0800

    update module

---
+++ mongodb/src/main/java/com/training/mongodb/MongoDBConnectionTest.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.training.mongodb;
+
+/**
+ * @author yangkai
+ *
+ */
+public class MongoDBConnectionTest {
+
+    /**
+     * 
+     */
+    public MongoDBConnectionTest() {
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
