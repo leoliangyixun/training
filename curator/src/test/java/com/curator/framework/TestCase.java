d52181c9f3ebc29b118cf91f047162f85471428a
季晴川 <jiqingchuan@b5m.com>
2016-05-19 18:49:06 +0800

    多线程2

---
+++ curator/src/test/java/com/curator/framework/TestCase.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package com.curator.framework;
+
+import java.io.IOException;
+import java.net.BindException;
+
+import org.apache.curator.test.TestingCluster;
+import org.apache.curator.test.TestingServer;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+/**
+ * @description:
+ * Copyright 2011-2015 B5M.COM. All rights reserved
+ * @author: jiqingchuan
+ * @version: 1.0
+ * @createdate: 2016年5月19日
+ * Modification  History:
+ * Date         Author        Version        Discription
+ * -----------------------------------------------------------------------------------
+ * 2016年5月19日       jiqingchuan          1.0             Why & What is modified
+ */
+public class TestCase {
+    
+    protected TestingServer server;
+    protected TestingCluster cluster;
+    
+    @BeforeMethod
+    public void setup() throws Exception {
+        System.out.println("---------------------setup()----------------------");
+        if (server == null) {
+            try {
+                server = new TestingServer();
+            } catch (BindException e) {
+                e.printStackTrace();
+                server = null;
+            }
+        }
+    }
+
+    @AfterMethod
+    public void teardown() throws Exception {
+        System.out.println("---------------------teardown()----------------------");
+        if (server != null) {
+            try {
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            server = null;
+        }
+    }
+
+}
--- test/src/main/java/com/b5m/training/concurrent/TestCollection.java
+++ test/src/main/java/com/b5m/training/concurrent/TestCollection.java
@@ -1,7 +1,9 @@
 package com.b5m.training.concurrent;
 
+import java.util.Deque;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Queue;
 import java.util.Set;
 
 /**
@@ -12,6 +14,7 @@ public class TestCollection {
     public static void  main(String[] args) {
         Set<String> set = new HashSet<String>();
         Iterator<String> it =set.iterator();
+        
     }
 
     public static class Task extends Thread{
