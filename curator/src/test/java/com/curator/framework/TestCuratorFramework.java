26472af384a77f0b74aafda093300207a4d47ef7
季晴川 <jiqingchuan@b5m.com>
2016-06-01 14:12:07 +0800

    update curator

--- curator/src/test/java/com/curator/framework/TestCuratorFramework.java
+++ curator/src/test/java/com/curator/framework/TestCuratorFramework.java
@@ -3,8 +3,26 @@
  */
 package com.curator.framework;
 
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.api.CuratorWatcher;
+import org.apache.curator.framework.api.GetChildrenBuilder;
+import org.apache.curator.framework.state.ConnectionState;
+import org.apache.curator.framework.state.ConnectionStateListener;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.curator.retry.RetryOneTime;
+import org.apache.curator.test.Timing;
+import org.apache.curator.utils.CloseableUtils;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.testng.Assert;
+
 import static org.junit.Assert.*;
 
+import java.util.List;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
@@ -39,8 +57,9 @@ public class TestCuratorFramework extends TestCase {
     
     @Test
     public void testConnectionState() throws Exception {
-  
-        CuratorFramework client = CuratorFrameworkFactory.newClient(server.getConnectString(), new RetryOneTime(1));
+
+        Timing timing = new Timing();
+        CuratorFramework client = CuratorFrameworkFactory.newClient(server.getConnectString(), timing.session(), timing.connection(), new RetryOneTime(1));
         try {
            
             ConnectionStateListener listener = new ConnectionStateListener() {
@@ -51,10 +70,8 @@ public class TestCuratorFramework extends TestCase {
             };
             client.getConnectionStateListenable().addListener(listener);
             client.start();
-          
             Thread.sleep(4000);
-           
-            server.stop(); 
+            server.stop();
             Thread.sleep(4000);
            
         } finally {
@@ -62,39 +79,36 @@ public class TestCuratorFramework extends TestCase {
           
         }
     }
-    
     @Test
-    public void testConnectionState2() throws Exception
-    {
-        Timing timing = new Timing();
-        CuratorFramework client = CuratorFrameworkFactory.newClient(server.getConnectString(), timing.session(), timing.connection(), new RetryOneTime(1));
-        try
-        {
-            final BlockingQueue<ConnectionState> queue = new LinkedBlockingQueue<ConnectionState>();
-            ConnectionStateListener listener = new ConnectionStateListener()
-            {
-                @Override
-                public void stateChanged(CuratorFramework client, ConnectionState newState)
-                {
-                    queue.add(newState);
-                    System.out.println("connenction state changed to " + newState.name());
-                }
-            };
-            client.getConnectionStateListenable().addListener(listener);
+    public void testNamespaceWithWatcher() throws Exception {
+        CuratorFramework client = CuratorFrameworkFactory.builder()
+                .connectString(this.server.getConnectString())
+                .namespace("curator.training")
+                .retryPolicy(new ExponentialBackoffRetry(1000, 3))
+                .connectionTimeoutMs(1000)
+                .sessionTimeoutMs(1000)
+                .build();
 
-            client.start();
-            //Assert.assertEquals(queue.poll(timing.multiple(4).seconds(), TimeUnit.SECONDS), ConnectionState.CONNECTED);
-            Thread.sleep(4000);
-            server.stop();
-            Thread.sleep(4000);
-            Thread.sleep(4000);
-            //Assert.assertEquals(queue.poll(timing.multiple(4).seconds(), TimeUnit.SECONDS), ConnectionState.SUSPENDED);
-            //Assert.assertEquals(queue.poll(timing.multiple(4).seconds(), TimeUnit.SECONDS), ConnectionState.LOST);
-        }
-        finally
-        {
-            CloseableUtils.closeQuietly(client);
-        }
+        client.start();
+
+        CuratorWatcher watcher = new CuratorWatcher() {
+            @Override
+            public void process(WatchedEvent event) {
+                System.out.println("watcher works ......" + event.getPath());
+            }
+        };
+        client.create().forPath("/base");
+        client.getChildren().usingWatcher(watcher).forPath("/base");
+        client.create().forPath("/base/child");
+        client.getChildren().usingWatcher(watcher).forPath("/base/child");
+        client.create().forPath("/base/child2");
+
     }
+
+    
+
+
+
+
 
 }
