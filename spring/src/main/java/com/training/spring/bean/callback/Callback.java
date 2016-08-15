d5ad30541df74a97d24b4234b9b6f7d0779652d2
yangkai <yangkai@hujiang.com>
2016-08-15 20:02:17 +0800

    update mybatis

---
+++ spring/src/main/java/com/training/spring/bean/callback/Callback.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.training.spring.bean.callback;
+
+
+/**
+ * @description:
+ * Copyright 2011-2015 B5M.COM. All rights reserved
+ * @author: jiqingchuan
+ * @version: 1.0
+ * @createdate: 2016年4月28日
+ * Modification  History:
+ * Date         Author        Version        Discription
+ * -----------------------------------------------------------------------------------
+ * 2016年4月28日       jiqingchuan          1.0             Why & What is modified
+ */
+public interface Callback<T> {
+    
+    T execute();
+    /*
+     * ?  在实现类中可以是任意类型
+     */
+    Class<?> getClassType();
+    
+    /*
+     * T  在实现类中只能是泛型 T
+     */
+    Class<T> getClassType4Generic();
+}
---
+++ spring/src/main/java/com/training/spring/bean/model/Employee.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package com.training.spring.bean.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElements;
+import javax.xml.bind.annotation.XmlList;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * @description:
+ * Copyright 2011-2015 B5M.COM. All rights reserved
+ * @author: jiqingchuan
+ * @version: 1.0
+ * @createdate: 2016年4月27日
+ * Modification  History:
+ * Date         Author        Version        Discription
+ * -----------------------------------------------------------------------------------
+ * 2016年4月27日       jiqingchuan          1.0             Why & What is modified
+ */
+public class Employee implements Serializable {
+
+
+	private String name;
+	private Integer age;
+
+	public Employee() {
+		// TODO Auto-generated constructor stub
+	}
+	/**
+	 * @return the name
+	 */
+	@XmlElement
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the age
+	 */
+	@XmlElement
+	public Integer getAge() {
+		return age;
+	}
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	
+
+}
