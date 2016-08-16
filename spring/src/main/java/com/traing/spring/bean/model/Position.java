5e538ec0ba7931e39f8132b27ed84f3b5ea6abb9
yangkai <yangkai@hujiang.com>
2016-08-16 12:47:11 +0800

    update

---
+++ spring/src/main/java/com/traing/spring/bean/model/Position.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.traing.spring.bean.model;
+
+/**
+ * @description:
+ * Copyright 2011-2015 B5M.COM. All rights reserved
+ * @author: jiqingchuan
+ * @version: 1.0
+ * @createdate: 2016年4月28日
+ * Modification  History:
+ * Date         Author        Version        Discription
+ * -----------------------------------------------------------------------------------
+ * 2016年4月28日       jiqingchuan          1.0             Why & What is modified
+ */
+public class Position {
+
+    /**
+     * 
+     */
+    public Position() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
